13444d3e7568cf06bd3ff98f8dd2c7c8e6cb4770
aleksanderbrymora <aleksander.australia@gmail.com>
2020-08-17 19:36:22 +1000

    succesfully (probably) converted to using context for hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useReadCollection } from './firebase-hooks/firestore/useReadCollection';
 
 const App: React.FC = () => {
-	return <div className='App'>stuff</div>;
+	const [error, loading, data] = useReadCollection('posts');
+	return (
+		<div className='App'>
+			{error && JSON.stringify(error)}
+			{loading ? <p>Loading...</p> : <p>{JSON.stringify(data)}</p>}
+		</div>
+	);
 };
 
 export default App;
