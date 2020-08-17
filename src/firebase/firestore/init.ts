3b0fb9f111041b5281d05543ab23351a2d69426c
aleksanderbrymora <aleksander.australia@gmail.com>
2020-08-17 18:58:22 +1000

    trying out stuff

--- src/firebase/firestore/init.ts
+++
@@ -1,9 +0,0 @@
-import 'firebase/firestore';
-import firebase from '../config/firebase-init';
-
-const firestore = firebase.firestore();
-
-// comment out if you want to disable offline support (eg: handling sensitive data)
-firestore.enablePersistence().catch((err: Error) => console.error(err));
-
-export default firestore;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { CreateFirebaseContext } from './firebase/context/CreateFirebaseContext';
+
+const Firebase = CreateFirebaseContext(
+	{
+		apiKey: 'AIzaSyBSRTRgDkPvJvt9Ub-1ttCSaUopOPimv80',
+		authDomain: 'hooksies123.firebaseapp.com',
+		databaseURL: 'https://hooksies123.firebaseio.com',
+		projectId: 'hooksies123',
+		storageBucket: 'hooksies123.appspot.com',
+		messagingSenderId: '705806228715',
+		appId: '1:705806228715:web:02ec668242568d339a72ae',
+		measurementId: 'G-P6TEK34DPZ',
+	},
+	['auth', 'database', 'firestore', 'storage'],
+);
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Firebase>
+			<App />
+		</Firebase>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
