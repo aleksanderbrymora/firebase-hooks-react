f19efddc824def33fd25c5f37be273a3b9108373
aleksanderbrymora <aleksander.australia@gmail.com>
2020-08-18 13:44:09 +1000

    small todo comment

--- src/firebase-hooks/types/firestore-data.ts
+++ src/firebase-hooks/types/firestore-data.ts
@@ -1,7 +1,7 @@
 export type CollectionData = [
 	Error | null,
 	boolean,
-	firebase.firestore.DocumentData | firebase.firestore.DocumentData[] | null,
+	firebase.firestore.DocumentData | firebase.firestore.DocumentData[] | null, // this might be a bit broken
 ];
 
 export type DocData = [Error | null, boolean, firebase.firestore.DocumentData];
